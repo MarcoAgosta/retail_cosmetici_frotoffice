27c19e9bd0314287f470f3e46ee4a1d6b79e3917
MarcoAgosta <agostamarco@libero.it>
2023-03-30 15:50:06 +0200

    Creata pagina index

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomePage from "./pages/Home.vue";
-import PostsShowPage from "./pages/posts/Show.vue";
+import IndexPage from "./pages/IndexPage.vue";
 // ... import di altri componenti
 
 const router = createRouter({
   
   history: createWebHistory(),
   routes: [
-    
+    {
+        path:"/",
+        name:"index",
+        component: IndexPage,
+    }
   ]
 });
 
