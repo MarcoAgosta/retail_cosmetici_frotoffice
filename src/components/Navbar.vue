a80d73af1d3ba3858e8ea88ff9f361106d5b28a1
MarcoAgosta <agostamarco@libero.it>
2023-04-01 17:55:24 +0200

    Creata nav-bar e iniziata estetica index

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+<nav class="navbar navbar-light">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center " href="#">
+          <h3 class="d-inline-block m-0">Profumeria</h3>
+          <img src="Logo-removebg-preview.png" class="d-inline-block align-top logo" alt="Logo del sito">
+        </a>
+    </div>
+</nav>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.logo{
+    width: 70px;
+}
+.navbar{
+    position: sticky;
+    background-color: white;
+}
+
+h3{
+    color: purple;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,30 @@
 <template>
-<h1>Pagina index</h1>
+<div class="background-purple">
+    <Navbar></Navbar>
+    <div class="container pt-4">
+        <h1>Profumi in catalogo</h1>
+        <div class="row g-3">
+            <div v-for="perfume in perfumes" class="col-4 p-3 d-flex flex-column align-items-center perfume">
+                <img :src="backendURL + perfume.product_img" class="w-50" alt="">
+                <h4>{{ perfume.name }}</h4>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar.vue";
 import { reactive } from "vue";
 import axios from "axios";
 export default{
     data(){
         return{
-            perfumes:[]
+            perfumes:[],
+            backendURL:"http://127.0.0.1:8000/storage/"
         }
     },
+    components:{Navbar},
     methods:{
         fetchAllPerfumes(){
             axios.get("http://localhost:8000/api/perfumes").then((resp)=>{
@@ -24,3 +38,16 @@ export default{
     }
     }
 </script>
+
+<style scoped>
+.background-purple{
+background-color: purple;
+min-height: 100vh;
+}
+.container{
+    background-color: white;
+}
+.perfume{
+border: 1px solid darkgray;
+}
+</style>
