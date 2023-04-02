f96eaa6b84f11e918a87a79b6b08dc9eaf9cb959
MarcoAgosta <agostamarco@libero.it>
2023-04-02 17:22:31 +0200

    Aggiunta la marca e corretta la carta

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,14 +2,23 @@
 <div class="background-purple">
     <Navbar></Navbar>
     <div class="container">
-        <h1>Profumi in catalogo</h1>
-        <div class="row gx-3">
-            <div v-for="perfume in perfumes" class="col-12 col-sm-6 col-md-4 col-xl-3 p-2">
-                <div class="d-flex flex-column align-items-center border bg-light carta justify-content-between p-2">
-                    <img :src="backendURL + perfume.product_img" class="h-75 img-carta" alt="">
-                    <h4 class="m-0 text-center purple">{{ perfume.name }}</h4>
+        <div v-if="perfumes[0]" class="row gx-3">
+            <h1 class="purple">Profumi in catalogo</h1>
+            <div v-for="perfume in perfumes" class="col-6 col-md-4 col-xl-3 p-2">
+                <div class="d-flex flex-column carta justify-content-between p-2">
+                    <div class="d-flex justify-content-center flex-column">
+                        <img :src="backendURL + perfume.product_img" class="w-75 mb-1 img-carta" alt="">
+                    </div>
+
+                    <div>
+                        <h4 class="m-0 purple">{{ perfume.name }}</h4>
+                        <span class="brand">{{ perfume.brand }}</span>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div v-else class="d-flex flex-column align-items-center justify-content-center loader-contenitore">
+            <div class="loader"></div>
         </div>
     </div>
 </div>
@@ -43,21 +52,44 @@ export default{
 
 <style scoped>
 .background-purple{
-background-color: purple;
+background-color: rgba(182, 0, 182, 0.699);
 min-height: 100vh;
 }
 .container{
-    background-color: white;
+    background-color: rgb(252, 245, 246);
     padding-top: 96px;
 }
 .carta{
-    height: 250px;
+    max-height: 300px;
 }
 .img-carta{
     max-width: 100%;
+    border-radius: 5%;
 }
 
 .purple{
     color: purple;
+}
+
+.loader {
+  border: 16px solid white; 
+  border-top: 16px solid purple; 
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.loader-contenitore{
+    height: 90vh;
+}
+.brand{
+font-family: initial;
+color: gray;
 }
 </style>
