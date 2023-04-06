0e34111541eb0613fb10709ec9a29f9270c48a03
MarcoAgosta <agostamarco@libero.it>
2023-04-06 17:22:49 +0200

    Fixate le immagini

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,33 +2,55 @@
 <div class="background-purple">
     <Navbar></Navbar>
     <div class="container">
-        <div v-if="perfumes[0]" class="row gx-3">
+        <div class v-if="noResults==true">
             <div class="row">
                 <h1 class="purple col-6">Profumi in catalogo</h1>
-                <div>
-                    <input v-model="filter" @click="filterPage(filter)" type="text" class="searchbar col-10 col-lg-6 p-2 mt-2 mb-2" placeholder="Cerca un prodotto">
+            </div>
+
+            <div class="row">
+                <input v-model="filter" type="text" class="searchbar col-10 col-lg-6 p-2 mt-2 mb-2" placeholder="Cerca un prodotto">
+                <div class="col-2 d-flex m-2 align-items-center">
+                    <button @click="filterPage(filter)" class="btn btn-purple m-0">Cerca</button>
                 </div>
             </div>
-            <div v-for="perfume in perfumes" class="col-6 col-md-4 col-xl-3 p-2">
-                <div class="d-flex flex-column carta justify-content-between p-2">
-                    <div class="d-flex justify-content-center flex-column">
-                        <img :src="backendURL + perfume.product_img" class="w-75 mb-1 img-carta" alt="">
-                    </div>
+            <div class="d-flex justify-content-center align-items-center mt-5 pt-5 purple">
+                <h3>Non ci sono risultati</h3>
+            </div>
+        </div>
 
-                    <div>
-                        <h4 class="m-0 purple">{{ perfume.name }}</h4>
-                        <span class="brand">{{ perfume.brand }}</span>
-                    </div>
+        <div v-else-if="perfumes[0]" class="row gx-3">
+            <div class="row">
+                <h1 class="purple col-6">Profumi in catalogo</h1>
+            </div>
+
+            <div class="row">
+                <input v-model="filter" type="text" class="searchbar col-10 col-lg-6 p-2 mt-2 mb-2" placeholder="Cerca un prodotto">
+                <div class="col-2 m-2 d-flex align-items-center">
+                    <button @click="filterPage(filter)" class="btn btn-purple m-0">Cerca</button>
                 </div>
+            </div>
+            
+            <div v-for="perfume in perfumes" class="col-6 col-md-4 col-xl-3 p-4 d-flex flex-column carta justify-content-between">
+
+                <div class="d-flex justify-content-center align-items-center flex-column max-heigth">
+                    <img :src="backendURL + perfume.product_img" class="mb-1 img-carta" alt="">
+                </div>
+
+                <div>
+                    <h4 class="m-0 purple">{{ perfume.name }}</h4>
+                    <span class="brand">{{ perfume.brand }}</span>
+                </div>
+
             </div>
             <div class="mb-4 d-flex col-12 justify-content-end">
                 <button v-if="btnPrev" @click="prevpage" class="btn btn-purple m-1">Precedente</button>
-                <button v-else class="btn btn-purple" disabled>Precedente</button>
+                <button v-else class="btn btn-purple m-1" disabled>Precedente</button>
 
                 <button v-if="btnNext" @click="nextpage" class="btn btn-purple m-1">Avanti</button>
-                <button v-else class="btn btn-purple" disabled>Avanti</button>
+                <button v-else class="btn btn-purple m-1" disabled>Avanti</button>
             </div>
         </div>
+        
         <div v-else class="d-flex flex-column align-items-center justify-content-center loader-contenitore">
             <div class="loader"></div>
         </div>
@@ -49,7 +71,8 @@ export default{
             filteredPerfumes:[],
             btnNext:"",
             btnPrev:"",
-            backendURL:"http://127.0.0.1:8000/storage/"
+            backendURL:"http://127.0.0.1:8000/storage/",
+            noResults: false,
         }
     },
     components:{Navbar},
@@ -79,6 +102,7 @@ export default{
             })
         },
         filterPage(argoument){
+            this.noResults=false,
             this.perfumes=[],
             axios.get("http://localhost:8000/api/perfumes", {
                 params: {
@@ -87,9 +111,11 @@ export default{
             }).then((resp)=>{
                 this.response=resp.data
                 this.perfumes=resp.data.results.data
-                console.log(this.perfumes)
                 this.btnNext=resp.data.results.next_page_url
                 this.btnPrev=resp.data.results.prev_page_url
+                if (this.perfumes.length==0){
+                    this.noResults=true
+                }
             })
         }
     },
@@ -100,17 +126,23 @@ export default{
 </script>
 
 <style scoped>
+.max-heigth{
+    max-height: 77%;
+    overflow: hidden;
+}
 .searchbar{
     border-radius: 1rem;
     border: none;
     mask-border: none;
     border-style: none;
+    margin-right: 1rem;
+    margin-left: 1rem;
 }
 .searchbar:focus{
     outline: none;
 }
 .background-purple{
-background-color: rgba(182, 0, 182, 0.699);
+background-color: rgba(255, 159, 255, 0.699);
 min-height: 100vh;
 }
 .container{
@@ -124,6 +156,8 @@ min-height: 100vh;
 .img-carta{
     max-width: 100%;
     border-radius: 5%;
+    height: 100%;
+    object-fit:cover;
 }
 
 .purple{
